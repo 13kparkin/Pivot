0bf41ed224a54dd78819df52b65049a89184f199
Kyle Parkin <13ktparkin@gmail.com>
2026-08-17 18:31:49 +0100

    refactor(server): consolidate duplicate omp barrel imports in ProviderRegistry

--- apps/server/src/provider/Layers/ProviderRegistry.ts
+++ apps/server/src/provider/Layers/ProviderRegistry.ts
@@ -54,9 +54,13 @@ import {
 import type { ProviderInstance } from "../ProviderDriver.ts";
 import { makeManualOnlyProviderMaintenanceCapabilities } from "../providerMaintenance.ts";
 import type { ProviderSnapshotSource } from "../builtInProviderCatalog.ts";
-import { OmpAdapter } from "../omp/index.ts";
+import {
+  listOmpLoginProviders,
+  loginOmpProvider,
+  OmpAdapter,
+  OmpLoginError,
+} from "../omp/index.ts";
 import { OmpCapabilitiesError } from "@t3tools/contracts";
-import { listOmpLoginProviders, loginOmpProvider, OmpLoginError } from "../omp/index.ts";
 import * as Crypto from "effect/Crypto";
 import * as Schema from "effect/Schema";
 
